e43366578318c164e5bd805c4738bc26123233b8
Dhwanan Mirani <dkm11@sfu.ca>
2025-10-25 23:00:09 -0700

    lab6 files added

---
+++ lab6/lab6.c
@@ -0,0 +1,98 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ASSERT(expr)                                                           \
+  {                                                                            \
+    if (!(expr)) {                                                             \
+      fprintf(stderr, "Assertion failed: %s\n", #expr);                        \
+      fprintf(stderr, "  at %s:%d\n", __FILE__, __LINE__);                     \
+      exit(1);                                                                 \
+    }                                                                          \
+  }
+
+#define TEST(expr)                                                             \
+  {                                                                            \
+    if (!(expr)) {                                                             \
+      fprintf(stderr, "Test failed: %s\n", #expr);                             \
+      exit(1);                                                                 \
+    } else {                                                                   \
+      printf("Test passed: %s\n", #expr);                                      \
+    }                                                                          \
+  }
+
+typedef struct node {
+  uint64_t data;
+  struct node *next;
+} node_t;
+
+node_t *head = NULL;
+
+uint64_t sum_list() {
+  uint64_t sum = 0;
+  node_t *curr = head;
+  while (curr != NULL) {
+    sum += curr->data;
+    curr = curr->next;
+  }
+  return sum;
+}
+
+void insert_sorted(uint64_t data) {
+  node_t *new_node = malloc(sizeof(node_t));
+  new_node->data = data;
+  new_node->next = NULL;
+
+  if (head == NULL || data < head->data) {
+    new_node->next = head;
+    head = new_node;
+  } else {
+    node_t *curr = head;
+
+    while (curr->next != NULL && data > curr->next->data) {
+      curr = curr->next;
+    }
+
+    new_node->next = curr->next;
+    curr->next = new_node;
+  }
+}
+
+int index_of(uint64_t data) {
+  node_t *curr = head;
+  int index = 0;
+
+  while (curr != NULL) {
+    if (curr->data == data) {
+      return index;
+    }
+
+    curr = curr->next;
+    index++;
+  }
+  return -1;
+}
+
+int main() {
+  insert_sorted(1);
+  ASSERT(sum_list() == 1);
+  insert_sorted(2);
+  ASSERT(sum_list() == 3);
+  insert_sorted(5);
+  ASSERT(sum_list() == 8);
+  insert_sorted(3);
+  ASSERT(sum_list() == 11);
+
+  TEST(index_of(3) == 2);
+
+  insert_sorted(0);
+  ASSERT(sum_list() == 11);
+  insert_sorted(4);
+  ASSERT(sum_list() == 15);
+
+  TEST(index_of(4) == 4);
+
+  printf("All assertions and tests passed!\n");
+  return 0;
+}
